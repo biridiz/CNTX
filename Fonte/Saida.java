fc6cc962efa05e54a00aca60706c19b0dfacc9e5
Luiz Dendena <luiz.dendena@gmail.com>
2019-12-24 18:33:45 -0200

    Reconhecendo variaveis no print

--- Fonte/Saida.java
+++ Fonte/Saida.java
@@ -1,23 +1,35 @@
 class Saida{
 
-	private String print_var;
-	private String print_texto;
+	public double print_var;
+	public String print_texto = new String();
 
-	public void analisePrint(char[] c){
-		
+	public Saida analisePrint(char[] c, Variavel[] v, String s){
+
 		for(int j=0; j<c.length; j++){
 			if(c[j] == '('){
 				j++;
 				if(c[j] == '"'){
 					j++;
 					while(c[j] != '"'){
-						System.out.print(c[j]);
-						j++;
+						this.print_texto += c[j];
+						j++;	
 					}
 				}
 			}
 		}
-		System.out.print("\n");
-		return;
+
+		for(int i=0; i<v.length; i++){
+			if(s.contains(v[i].getName())){
+				for(int k=0; k<c.length; k++){
+					if(c[k] == '+'){
+						this.print_var = v[i].getValue();
+						break;
+					}
+				}
+				break;
+			}
+		}
+
+		return this;
 	}
 }
