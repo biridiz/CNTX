c14048f8809c9ae185c8d9f5cabd0d17cf1e9b0c
Luiz Dendena <luiz.dendena@gmail.com>
2019-12-26 13:32:32 -0200

    Funçoes de calculo para operações

--- Fonte/Matematica.java
+++ Fonte/Matematica.java
@@ -1,30 +1,59 @@
 class Matematica{
-	
-	private Variavel[] var;
 
-	public double mat(double a, double b, String c){
-		double r = 0;
+	public void analiseOp(char[] c, Variavel[] v, String s){
+		
+		char op = ' ';
+		double[] cash = new double[3];
+		int j = 0;
+		
+		for(int i=0; i<c.length; i++){
+			if(
+				c[i] == '+' ||
+				c[i] == '-' ||
+				c[i] == '*' ||
+				c[i] == '/' ||
+				c[i] == '%'
+			){
+				op = c[i];
+				break;
+			}
+		}
 
-		if(c.equals("+")){
+		for(int i=0; i<cash.length; i++){
+			if(s.contains(v[i].getName())){
+				cash[j] = v[i].getValue();
+				j++;
+			}
+		}
+
+		cash[0] = this.math(cash[1], cash[2], op);
+		v[0].setValue(cash[0]);
+		System.out.println(cash[0]);
+		return;
+	}
+
+	public double math(double a, double b, char c){
+		
+		double r = 0.0;
+
+		if(c == '+'){
 			r = a+b;
 		}
-		else if(c.equals("-")){
+		else if(c == '-'){
 			r = a-b;
 		}
-		else if(c.equals("*")){
+		else if(c == '*'){
 			r = a*b;
 		}
-		else if(c.equals("/")){
+		else if(c == '/'){
 			if(a != 0 || b != 0){
 				r = a/b;
 			}
 		}
-		else if(c.equals("%")){
+		else if(c == '%'){
 			r = a%b;
 		}
 
 		return r;
-	}
-
-	
+	}	
 }
