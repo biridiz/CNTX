8885e885fe1427d25e1d8938e6bf97e32ea9e9ba
Luiz Dendena <luiz.dendena@gmail.com>
2019-12-27 18:55:01 -0200

    Testando funcionalidades de condicional

--- Fonte/Cond.java
+++ Fonte/Cond.java
@@ -1,7 +1,54 @@
 class Cond{
 
-	//private Variavel[] var;
-	private boolean ok;
+	private boolean ok = false;
+
+	public boolean analiseCond(char[] c, Variavel[] v, String s){
+
+		String op = new String();
+		double[] cash = new double[3];
+		int j = 0;
+		
+		if(s.contains("==")){
+			op = "==";
+		}
+		else if(s.contains("<")){
+			op = "<";
+		}
+		else if(s.contains(">")){
+			op = ">";
+		}
+		else if(s.contains("!=")){
+			op = "!=";
+		}
+		else if(s.contains("<=")){
+			op = "<=";
+		}
+		else if(s.contains(">=")){
+			op = ">=";
+		}
+		else{
+			return ok;
+		}
+
+		for(int i=0; i<=v.length; i++){
+			if(j >= 2){
+				break;
+			}
+			else{
+				if(v[i] != null){
+					if(s.contains(v[i].getName()) == true){
+						cash[j] = v[i].getValue();
+						j++;
+					}
+				}
+				else{
+					break;
+				}
+			}
+		}
+		ok = this.se(cash[0], cash[1], op);
+		return ok;
+	}
 
 	public boolean se(double a, double b, String c){
 
